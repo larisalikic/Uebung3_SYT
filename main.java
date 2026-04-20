5879224f4bdfc9b1e0b4a4e2fd79f5e56093a01b
Larisa Likic <larisa.likic@gmail.com>
2026-04-20 13:00:43 +0200

    hallo

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main(){
   public static void main(String[] args){
     System.out.println("moin meister");
 	System.out.println("das is git");
+	System.out.println("hallo ich heiße larisa");
   }
 }
